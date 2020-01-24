ea757621a0d76b0dc0556cb46353d11b5db0a222
karolinadistasi <46462387+karolinadistasi@users.noreply.github.com>
2020-01-23 23:18:07 -0500

    cleaned up formatting

--- question_three.py
+++ question_three.py
@@ -2,18 +2,18 @@ import requests
 import pprint
 
 def get_public_repos(user):
-	url = 'https://api.github.com'
-	user_specs = f'/users/{user}/repos'
-	query_string_param = 'type=public'
+    base_url = 'https://api.github.com'
+    user_specs = f'/users/{user}/repos'
+    query_string_param = {'type': 'public'}
 
-	response = requests.get(url+user_specs+'?'+query_string_param)
+    response = requests.get(url = base_url + user_specs, params = query_string_param)
 
-	data = response.json()
-	# pp = pprint.PrettyPrinter(indent=2)
-	# pp.pprint(data)
+    data = response.json()
+    # pp = pprint.PrettyPrinter(indent=2)
+    # pp.pprint(data)
 
-	repo_names = []
-	for each in range(len(data)):
-			repo_names.append(data[each]['name'])
+    repo_names = []
+    for each in range(len(data)):
+        repo_names.append(data[each]['name'])
 
-	print(repo_names)
+    print(repo_names)
