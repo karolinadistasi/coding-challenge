eb8c67169b0499989cb141e7135575989edfee4b
karolinadistasi <46462387+karolinadistasi@users.noreply.github.com>
2020-01-23 23:56:54 -0500

    added user input to make function accessible through terminal

--- question_three.py
+++ question_three.py
@@ -6,14 +6,21 @@ def get_public_repos(user):
     user_specs = f'/users/{user}/repos'
     query_string_param = {'type': 'public'}
 
-    response = requests.get(url = base_url + user_specs, params = query_string_param)
+    response = requests.get(url=base_url+user_specs, params=query_string_param)
 
-    data = response.json()
-    # pp = pprint.PrettyPrinter(indent=2)
-    # pp.pprint(data)
+    if response.status_code == 200:
+        data = response.json()
+        # pp = pprint.PrettyPrinter(indent=2)
+        # pp.pprint(data)
 
-    repo_names = []
-    for each in range(len(data)):
-        repo_names.append(data[each]['name'])
+        print(f'Publicly accessible repositories for user {user}:\n')
+        for each in range(len(data)):
+            print(data[each]['name'])
+        
+        print()
+     
+    else:
+        print(f'Status code {response.status_code} received. User does not exist.')
 
-    print(repo_names)
+user = input('Enter GitHub user: ')
+get_public_repos(user)
